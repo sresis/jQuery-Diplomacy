349ebdc5023d9d73f44848f3cb7937827e813f8a
Stephanie Resis <sresis@umich.edu>
2020-07-29 16:38:29 -0700

    changed text color and updated number validation

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function changeColor() {
   const colorChangeEls = $('.color-change');
 
   for (const el of colorChangeEls) {
-    $(el).toggleClass('hide');
+    $(el).toggleClass('red');
   }
 }
 
@@ -17,7 +17,7 @@ function validateNumber(evt) {
   const userNum = parseInt(numberInput.val(), 10);  // typecast to num
 
   const formFeedback = $('#formFeedback');
-  if (!userNum) {
+  if (userNum > 10 || !userNum) {
     formFeedback.text('Please enter a smaller number');
   } else {
     formFeedback.text('You are good to go!');
